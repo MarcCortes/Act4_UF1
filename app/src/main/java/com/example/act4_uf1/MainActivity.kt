7dc2a7ad63e6a3b7b9f29a992764b16bc459e1f0
MarcCortes <marc7seda@gmail.com>
2024-10-21 21:48:09 +0200

    acabao

--- app/src/main/java/com/example/act4_uf1/MainActivity.kt
+++ app/src/main/java/com/example/act4_uf1/MainActivity.kt
@@ -1,11 +1,10 @@
-package com.example.botoapretat
+package com.example.act4_uf1
 
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.act4_uf1.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
